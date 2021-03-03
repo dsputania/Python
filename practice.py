c61cfb44d2a202eddb05cc2e2ac90cc06cd8216e
dsputania <dsputania@gmail.com>
2021-03-03 05:48:52 +0530

    number guessing game

--- practice.py
+++ practice.py
@@ -1,31 +1,56 @@
-rain_check = ""
-while rain_check != "no":
+while True:
     print("Good morning! Please go to your office.")
-
-    rain_check = input("Is it raining outside? (yes/no): ").lower().strip()
-    if rain_check == "yes":
-
-
-        umbrella_check = input("Do you have an umbrella? (yes/no): ").lower().strip()
-        if umbrella_check == "yes":
-            print("GO OUTSIDE.")
-            break
-
-        else:
-            print("WAIT A WHILE")
-
-
-            while True:
-                rain_check_again = input("Is it still raining? (yes/no) : ").lower().strip()
-                if rain_check_again == "yes":
-                    print("WAIT FOR SOME MORE TIME")
-                else:
-                    print("GO OUTSIDE")
-                    break
-
-    else:
+    rain_check = input("Is it raining outside?: ").strip().lower()
+    if rain_check == "no":
         print("GO OUTSIDE")
         break
+    elif rain_check == "yes":
+        while True:
+            umbrella_check = input("Do you have an umbrella?: ").strip().lower()
+            if umbrella_check == "yes":
+                print("GO OUTSIDE")
+                exit()
+            elif umbrella_check == "no":
+                while True:
+                    print("Wait for sometime and watch NETFLIX.")
+                    rain_check2 = input("Is it still raining?: ").strip().lower()
+                    if rain_check2 == "no":
+                        print("GO OUTSIDE")
+                        exit()
+                    elif rain_check2 == "yes":
+                        continue
+                    else:
+                        print("Please reply in yes or no only.")
+            else:
+                print("Please reply in yes or no only.")
+    else:
+        print("Please reply in yes or no only.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
